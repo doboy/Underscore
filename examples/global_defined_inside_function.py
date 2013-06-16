9244bb437fc6daecbe8fe0ff324db71c901fad6f
Huan Do <doboy0@gmail.com>
2013-06-16 04:28:11 -0700

    added a test case which previously failed

---
+++ examples/global_defined_inside_function.py
@@ -0,0 +1,6 @@
+def foo():
+    global x
+    x = 3
+
+foo()
+print x
---
+++ examples/underscored/global_defined_inside_function.py
@@ -0,0 +1,15 @@
+#  def foo():
+#      global x
+#      x = 3
+#  
+#  foo()
+#  print x
+
+(___,) = (3,)
+
+def _():
+    global __
+    __ = ___
+_()
+print __
+(foo, x) = (_, __)
