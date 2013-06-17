04cf09aeb2efda466ae5c943238271aaac75c37c
Huan Do <doboy0@gmail.com>
2013-06-17 00:39:40 -0700

    create an execfile function named xfile due to python3's depercation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,11 +7,16 @@ except:
     # Python 3 support
     from io import StringIO
 
+# Python 3 does not have execfile, so just create one
+def xfile(afile, globalz=None, localz=None):
+    with open(afile, "r") as fh:
+        exec(fh.read(), globalz, localz)
+
 def execute(filename):
     sys.stdout = fileOut = StringIO()
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        execfile(filename, {'__file__': filename})
+        xfile(filename, {'__file__': filename})
     sys.stdout = sys.__stdout__
     output = fileOut.getvalue()
     fileOut.close()
