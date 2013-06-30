b0606c4f2981d3f6c267d159d36e59bd548a08b9
Huan Do <doboy0@gmail.com>
2013-06-30 10:15:26 -0700

    missing change caused by a previous commit

--- examples/underscored/raise.py
+++ examples/underscored/raise.py
@@ -1,11 +1,11 @@
 #  try:
-#      raise AssertionError, 'this is a test', 'xx'
+#      raise AssertionError('this is a test')
 #  except:
 #      print('test passed')
 
-(__, ___, ____) = ('this is a test', 'xx', 'test passed')
+(__, ___) = ('this is a test', 'test passed')
 (_,) = (AssertionError,)
 try:
-    raise_, __, ___
+    raise_(__)
 except:
-    print ____
+    print ___
